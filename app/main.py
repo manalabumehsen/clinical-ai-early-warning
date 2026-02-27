baa0f19395c42b0418d9cd22aa816745476ace3d
manalabumehsen <manal.hafez1991@gmail.com>
2026-02-27 21:01:44 +0100

    Configure Streamlit page settings and call dashboard
    Set page title and layout for the Streamlit app.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from dashboard import show_dashboard  # بدون "app."
+from dashboard import show_dashboard 
 
+# إعداد الصفحة
 st.set_page_config(page_title="Clinical AI Early Warning Platform", layout="wide")
 st.title("💉 Clinical AI Early Warning Platform")
 
+# استدعاء الداشبورد
 show_dashboard()
